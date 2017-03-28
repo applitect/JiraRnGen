7d1a45dae5adad134a7bd096ee16f849e6fb0dd5
dthompson <dthompson@sofi.org>
2017-03-28 14:56:59 -0600

    Modified to use Mustache for templates for issues. Updated to allow releasing non-versioned release notes (using jql query).

--- release-notes.js
+++ release-notes.js
@@ -3,7 +3,9 @@ var fs = require("fs");
 var stdio = require("stdio");
 var logo = require("./image/logo.json");
 var sendmail = require("./util/sendmail");
+var template = fs.readFileSync("./template/issue-template.html", 'utf8');
 var juice = require('juice');
+var Mustache = require('mustache');
 
 var args = stdio.getopt({
     'email': {description: 'Send email'},
@@ -13,11 +15,13 @@ var args = stdio.getopt({
     'hostName' : {key: 'n', description: 'Jira Host [blah.atlassian.net]', mandatory: true, args: 1},
     'hostPort' : {key: 't', description: 'Port for Jira host', args:1},
     'context' : {key: 'c', description: 'Context for Jira Url', args: 1},
-    'fixVersion' : {key: 'f', description: 'Version name which to create release notes', mandatory: true, args: 1},
-    'component' : {key: 'm', description: 'Only generate release notes with specified comma separated component(s) listed', args: 1}
+    'fixVersion' : {key: 'f', description: 'Version name which to create release notes', mandatory: false, args: 1},
+    'jqlSearch' : {key: 'j', description: 'jql search which to create release notes', mandatory: false, args: 1},
+    'component' : {key: 'm', description: 'Only generate release notes with specified comma separated component(s) listed', args: 1},
+    'releaseName' : {key: 'r', description: 'The name of the release if not a fix version.', args: 1},
+    'template' : {key: 'e', description: 'The issue template.', args: 1}
 });
 
-var fixVersion = args.fixVersion;
 var context = "/";
 if (args.context) {
     context = '/' + args.context + '/';
@@ -35,14 +39,14 @@ function formatDate(date) {
     return date.substring(0,10);
 }
 
-function buildEmail(release, issues) {
+function buildEmail(releaseName, issues) {
     var issueType = {};
     var css = fs.readFileSync("stylesheet/style.css");
     var html = '<!DOCTYPE html> \n' +
         '<html> \n' +
         '<head> \n' +
         '<meta charset="UTF-8"> \n' +
-        '<title>Release Notes ' + release.name + '</title> \n' +
+        '<title>Release Notes ' + releaseName + '</title> \n' +
         '<style>' +
         css.toString() +
         '</style>\n' +
@@ -55,70 +59,18 @@ function buildEmail(release, issues) {
     }
     html +=
         '<h1>Release Notes</h1>\n' +
-        '<h4>Version : <span class="version">' + release.name + '</span></h4>\n' +
+        '<h4>Version : <span class="version">' + releaseName + '</span></h4>\n' +
         '<h4>Date : ' + new Date() + '</h4>\n';
-        if (!args.email) {
-            html += '<a href="#" class="expand-all">Expand All</a>';
-        }
 
     for(var i = 0, len = issues.length; i < len; i++) {
         var issue = issues[i];
         if (issue !== undefined) {
-            if (issue.changelog !== undefined) {
-                var hist = issue.changelog.histories;
-                var changers = {};
-                for (var c = 0; c < hist.length; c++) {
-                    var change = hist[c];
-                    changers[change.author.name] = 1;
-                }
-                issue.changers = Object.keys(changers);
-            }
-            issueType[issue.fields.issuetype.name] = issueType[issue.fields.issuetype.name] || [];
-            issueType[issue.fields.issuetype.name].unshift(issue);
-        }
-    }
-
-    var keys = Object.keys(issueType);
-    keys.sort();
-    for (var k = 0; k < keys.length; k++) {
-        var type = keys[k];
-        var typedIssues = issueType[type];
-        html += '<h3>' + type + '(s)</h3>\n';
-        for(i = 0, len = typedIssues.length; i < len; i++) {
-            var issue = typedIssues[i];
-            var link = 'https://' + args.hostName + '/browse/' + issue.key;
-            html +=
-                '<div class="issue" tabindex="0"><span class="issue-arrow"> &#8227; &nbsp; </span><a href="' + link + '">' + issue.key + '</a> - ' + issue.fields.summary + ' (<i>' + issue.changers + '</i>)</div>';
-            html += '<div class="child">\n' +
-                '<table>\n' +
-                '<td><span class="header">Priority:</span> ' + issue.fields.priority.name + '</td><td><span class="header">Components:</span> ';
-            if (issue.fields.components !== null) {
-                var comps = issue.fields.components;
-                for (var c = 0; c < comps.length; c++) {
-                    var comp = comps[c];
-                    html += '<span class="component">' + comp.name + '</span> ';
-                }
-            }
-            html += ' </td>\n' +
-                '</tr><tr>\n';
-            if (issue.fields.reporter.displayName !== null && issue.fields.creator.displayName === issue.fields.reporter.displayName) {
-                html += '<td colspan="2"><span class="header">Created by:</span> ' + issue.fields.creator.displayName + '</td>\n';
-            } else {
-                html += '<td><span class="header">Created by:</span> ' + issue.fields.creator.displayName + '</td><td><span class="header">Reported by:</span> ' + issue.fields.reporter.displayName + '</td>\n';
-
-            }
-            html += '</tr><tr>\n';
-            if (issue.fields.duedate !== null) {
-                html += '<td><span class="header">Created date:</span> ' + formatDate(issue.fields.created) + '</td><td><span class="header">Due date:</span> ' + issue.fields.duedate + '</td>\n';
-            } else {
-                html += '<td colspan="2"><span class="header">Created date:</span> ' + formatDate(issue.fields.created) + '</td>\n';
-            }
-            html += '</tr><tr>\n';
+            // Create a description with the markup changed to html.
             if (issue.fields.description !== null) {
                 // Replace all new lines with the null character so the description is on one line -- easier regex
                 var desc = issue.fields.description.replace(/(?:\r\n|\r|\n)/g, "\0");
                 // Replace all {code} blocks with <pre> blocks
-                var codeRegex = /(\{code\})(.*?)(\{code\})/g;
+                var codeRegex = /(\{code.*\})(.*?)(\{code\})/g;
                 desc = desc.replace(codeRegex, function(a,b,c) { return "<pre>" + c.replace(/\0/g, "\n") + "</pre>";});
                 // Surround all ordered lists with <ol> and within blocks replace nulls with new lines
                 desc = desc.replace(/\0([\t ]*#[^\0]*(\0[\t ]*#[^\0]*)*)/g, "\n<ol>\n$1\n</ol>\n");
@@ -146,13 +98,35 @@ function buildEmail(release, issues) {
                 // Find all block quotes, lines starting with bq. and wrap <quote> around them.
                 desc = desc.replace(/[\t ]*bq[.] (.*)/, "<quote>$1</quote>");
 
-                html += '<td colspan="2" class="description"><span class="header">Description: </span><div>' + desc + '</div></td>\n';
-            } else {
-                 html += '<td colspan="2"><span class="header">Description:</span> <i>None</i></td>\n';
+                issue.description = desc;
             }
-            html += '</tr></table>' +
-                '</div>\n';
+
+            issue.fields.created = formatDate(issue.fields.created);
+
+            if (issue.changelog !== undefined) {
+                var hist = issue.changelog.histories;
+                var changers = {};
+                for (var c = 0; c < hist.length; c++) {
+                    var change = hist[c];
+                    changers[change.author.name] = 1;
+                }
+                issue.changers = Object.keys(changers);
+            }
+            issueType[issue.fields.issuetype.name] = issueType[issue.fields.issuetype.name] || [];
+            issueType[issue.fields.issuetype.name].unshift(issue);
         }
+    }
+
+    var keys = Object.keys(issueType);
+    keys.sort();
+
+    for (var k = 0; k < keys.length; k++) {
+        var type = keys[k];
+        var typedIssues = issueType[type];
+        html += '<h3>' + type + '(s)</h3>\n';
+        var rendered = Mustache.render(template, {issues: typedIssues});
+
+        html += rendered;
     }
     html += '</body>\n</html>';
 
@@ -164,7 +138,7 @@ function buildEmail(release, issues) {
     }
 }
 
-function getVersionInfo(id, issues) {
+function getVersionInfo(id, issues, fixVersion) {
     options.path = context + 'rest/api/2/version/' + id;
     var request = https.get(options, function(res) {
         var body = "";
@@ -173,7 +147,7 @@ function getVersionInfo(id, issues) {
         });
         res.on('end', function() {
             var json = JSON.parse(body);
-            var email = buildEmail(json, issues);
+            var email = buildEmail(json.name, issues);
             if (args.email) {
                 if (args.sendTo === undefined) {
                     console.log("to send email you must set sendTo");
@@ -190,34 +164,81 @@ function getVersionInfo(id, issues) {
     });
 }
 
-// Set up the Jira search function to locate the specific issues to be reported
-options.path = context + 'rest/api/2/search?jql=fixVersion=' + encodeURI(fixVersion);
-if (args.component) {
-    options.path = options.path + encodeURI(' AND component in (' + args.component + ')');
+if (args.template !== undefined) {
+    template = fs.readFileSync(args.template, 'utf8');
 }
-options.path = options.path + '&expand=changelog';
 
-var request = https.get(options, function(res) {
-    if (res.statusCode < 200 || res.statusCode > 299) {
-        console.log("Unable to communicate with Jira instance. Status code: " + res.statusCode);
-        return;
+// Must have a fixVersion or a jqlSearch string
+if (args.fixVersion !== undefined) {
+    // Set up the Jira search function to locate the specific issues to be reported
+
+    options.path = context + 'rest/api/2/search?jql=fixVersion=' + encodeURI(args.fixVersion);
+    if (args.component) {
+        options.path = options.path + encodeURI(' AND component in (' + args.component + ')');
     }
-    var body = "";
-    res.on('data', function(data) {
-        body += data;
-    });
-    res.on('end', function() {
-        var json = JSON.parse(body);
-        var issues = json.issues;
-        if (issues === undefined || issues.length === 0) {
-            console.log("No issues found for version: " + fixVersion);
+    options.path = options.path + '&expand=changelog';
+
+    var request = https.get(options, function(res) {
+        if (res.statusCode < 200 || res.statusCode > 299) {
+            console.log("Unable to communicate with Jira instance. Status code: " + res.statusCode);
             return;
         }
-        var id = issues[0].fields.fixVersions[0].id;
-        getVersionInfo(id, issues);
+        var body = "";
+        res.on('data', function(data) {
+            body += data;
+        });
+        res.on('end', function() {
+            var json = JSON.parse(body);
+            var issues = json.issues;
+            if (issues === undefined || issues.length === 0) {
+                console.log("No issues found for version: " + args.fixVersion);
+                return;
+            }
+            var id = issues[0].fields.fixVersions[0].id;
+            getVersionInfo(id, issues, args.fixVersion);
+        });
+        res.on('error', function(e) {
+            console.log("Unable to communicate with Jira: " + e.message);
+        });
     });
-    res.on('error', function(e) {
-        console.log("Unable to communicate with Jira: " + e.message);
-    });
-});
+} else if (args.jqlSearch !== undefined && args.releaseName !== undefined) {
+    // Handle sending issues that return in a search but may not be in a version.
+    // This is for projects that close out tickets but don't assign versions to the tickets.
+    //Set up the Jira search function to locate the specific issues to be reported
+    options.path = context + 'rest/api/2/search?jql=' + encodeURI(args.jqlSearch);
+    options.path = options.path + '&expand=changelog';
 
+    var request = https.get(options, function(res) {
+        if (res.statusCode < 200 || res.statusCode > 299) {
+            console.log("Unable to communicate with Jira instance. Status code: " + res.statusCode);
+            return;
+        }
+        var body = "";
+        res.on('data', function(data) {
+            body += data;
+        });
+        res.on('end', function() {
+            var json = JSON.parse(body);
+            var issues = json.issues;
+            if (issues === undefined || issues.length === 0) {
+                console.log("No issues found for jqlSearch: " + args.jsqlSearch);
+                return;
+            }
+            var email = buildEmail(args.releaseName, issues);
+            if (args.email) {
+                if (args.sendTo === undefined) {
+                    console.log("to send email you must set sendTo");
+                    return;
+                }
+                sendmail.send(args.releaseName, args.sendTo, email, logo);
+            } else {
+                console.log(email);
+            }
+        });
+        res.on('error', function(e) {
+            console.log("Unable to communicate with Jira: " + e.message);
+        });
+    });
+} else {
+    console.log("Missing parameter: jqlSearch and releaseName or fixVersion.");
+}
